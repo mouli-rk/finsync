72af6c68111182d807869a1ca1733154aad685e0
Chandramouli P <chandramoulipydimalla@gmail.com>
2024-11-13 13:04:11 +0530

    Module Enhancements : 13 Nov 2024 13:04

--- finsync-user-service/src/main/java/in/syncuser/config/CustomLogoutHandler.java
+++ finsync-user-service/src/main/java/in/syncuser/config/CustomLogoutHandler.java
@@ -1,11 +1,16 @@
 package in.syncuser.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.syncuser.constants.FinSyncConstants;
 import in.syncuser.entity.Token;
+import in.syncuser.model.LoginModel;
 import in.syncuser.repository.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,14 +23,26 @@ public class CustomLogoutHandler implements LogoutHandler {
 
 	@Autowired
 	private TokenRepository tokenRepository;
+	
+	@Autowired
+	private ObjectMapper mapper;
 
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		String jwt = jwtUtils.getJwtFromHeader(request);
-		if (jwt != null && !jwt.isBlank()) {
-			Token token = tokenRepository.findByToken(jwt).orElseThrow();
-			token.setIsActive(false);
-			tokenRepository.save(token);
+		try {
+			String jwt = jwtUtils.getJwtFromHeader(request);
+			if (jwt != null && !jwt.isBlank()) {
+				Token token = tokenRepository.findByToken(jwt).orElseThrow();
+				token.setIsActive(false);
+				tokenRepository.save(token);
+				LoginModel model = new LoginModel();
+				model.setStatus(HttpStatus.OK.value());
+				model.setMessage(FinSyncConstants.SUCCESS);
+				mapper.writeValue(response.getWriter(), model);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
